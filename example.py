1285222038c8c2752d0572762f10efa678ec825e
Adalto Picotti Junior <adalto@principia.net>
2024-02-05 23:32:36 -0300

    config: Adding pre-commit to project

---
+++ example.py
@@ -0,0 +1,11 @@
+'''Documentação de módulo'''
+# Pylint
+# snake_case -> Funções, Variáveis, Métodos
+# PascalCase -> Classes
+
+def my_func():
+    '''
+        Documentação sobre a função
+    '''
+    print('Ola')
+    
